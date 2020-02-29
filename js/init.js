508b146ec8c24b62bee7430d31ddf659d999f367
lyadhcoder <srvmondal77@gmail.com>
2020-03-01 00:56:13 +0530

    initial commit

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-
+    $('.modal').modal();
+    $(".dropdown-trigger").dropdown();
+    $(".dropdown-trigger2").dropdown();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
