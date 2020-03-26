515711ff4c47539934390b93239b30674a603a2b
lyadhcoder <srvmondal77@gmail.com>
2020-03-27 03:39:17 +0530

    initial update

--- js/init.js
+++ js/init.js
@@ -6,5 +6,12 @@
     $('.modal').modal();
     $(".dropdown-trigger").dropdown();
     $(".dropdown-trigger2").dropdown();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true,
+    });
+    setInterval(function() {
+      $('.carousel').carousel('next');
+    }, 4000);
   }); // end of document ready
 })(jQuery); // end of jQuery name space
