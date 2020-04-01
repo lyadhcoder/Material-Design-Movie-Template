f493c0ad67464b66684a3896a0d8090aee701ce5
lyadhcoder <srvmondal77@gmail.com>
2020-04-01 14:24:10 +0530

    Initial Update & Subscribe Section Add

---
+++ owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav: false,
+    dots: false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:6
+        },
+        1000:{
+            items:7
+        }
+    }
+  })
